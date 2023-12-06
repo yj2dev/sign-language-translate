3647eb0a8225200012fe77bc9ff08f4e06e1fd8a
gilgagun <ksm2853305@gmail.com>
2023-12-06 13:17:52 +0900

    model: 기본 User 모델 추가

--- server/api/models.py
+++ server/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
-# Create your models here.
+ 
+ 
+class User(models.Model):
+    nickname = models.CharField(max_length=20, verbose_name='닉네임')
+    id = models.CharField(max_length=20, verbose_name='아이디', primary_key=True, unique=True)
+    password = models.CharField(max_length=20, verbose_name='비밀번호')
+   
+    class Meta:
+        db_table = 'User'
