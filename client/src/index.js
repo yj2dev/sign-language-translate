ac00a4c16223876dbff68c07d682e5bb67b6b1df
ad <nogo22@naver.com>
2023-12-07 02:14:56 +0900

    feat: ChatPage 디자인
    - 채팅창 디자인
    - 입력폼 디자인

--- client/src/index.js
+++ client/src/index.js
@@ -10,17 +10,19 @@ import getCookie from "./utils/Cookie";
 // axios.defaults.xsrfHeaderName = "x-CSRFToken";
 // axios.defaults.withCredentials = true;
 
-axios.defaults.baseURL = "http://localhost:8000"; // 개발 서버
+// axios.defaults.baseURL = "http://localhost:8000"; // 개발 서버
+
+axios.defaults.baseURL = "https://yujinlove.kro.kr"; // ssl, dongmin
 
 // axios.defaults.baseURL = "http://13.212.5.129"; // 운영 서버, dongmin ec2
 
 // axios.defaults.baseURL = "http://222.105.252.28:8080"; // 운영 서버, yujin school
 // axios.defaults.baseURL = "https://222.105.252.28:8080"; // 운영 서버 SSL, yujin school
 
-axios.defaults.withCredentials = true;
-axios.defaults.xsrfCookieName = "csrftoken";
-axios.defaults.xsrfHeaderName = "X-CSRFToken";
-axios.defaults.headers.common["X-CSRFToken"] = getCookie("csrftoken");
+// axios.defaults.withCredentials = true;
+// axios.defaults.xsrfCookieName = "csrftoken";
+// axios.defaults.xsrfHeaderName = "X-CSRFToken";
+// axios.defaults.headers.common["X-CSRFToken"] = getCookie("csrftoken");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
