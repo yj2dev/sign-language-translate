2bfa54c0b1e05494a7460f9bb557c3d83709469b
ad <nogo22@naver.com>
2023-12-06 12:59:25 +0900

    update: baseURL 수정

--- client/src/index.js
+++ client/src/index.js
@@ -14,7 +14,7 @@ import getCookie from "./utils/Cookie";
 
 // axios.defaults.baseURL = "http://13.212.5.129"; // 운영 서버, dongmin ec2
 
-axios.defaults.baseURL = "http://222.105.252.28:8080/"; // 운영 서버, yujin school
+axios.defaults.baseURL = "http://222.105.252.28:8080"; // 운영 서버, yujin school
 
 axios.defaults.withCredentials = true;
 axios.defaults.xsrfCookieName = "csrftoken";
