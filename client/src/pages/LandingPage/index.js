f90c2fcaa98a65a8eb55e5a7395e432d27d34953
lsj <lsj@gmail.com>
2023-12-06 13:40:02 +0900

    모델, 팀 소개 페이지

--- client/src/pages/LandingPage/index.js
+++ client/src/pages/LandingPage/index.js
@@ -1,11 +1,16 @@
 import { Container } from "./styled";
 import IntroduceSection from "./Section/IntroduceSection";
 import DemoSection from "./Section/DemoSection";
+import Sign_Lang_Model from "./Section/Sign_Lang_Model";
+import Team_info from "./Section/Team_info";
+
 const LandingPage = () => {
   return (
     <Container>
       <IntroduceSection />
       <DemoSection />
+      <Sign_Lang_Model />
+      <Team_info />
     </Container>
   );
 };
